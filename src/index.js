eda323b17e03711c4768bc203e7f288205f2af1e
max-violent <max.violent@gmail.com>
2018-09-24 13:07:29 +0300

    a

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	var br=bracketsConfig;
+	var arr=[];
+	out: for(var i=0; i<str.length; i++) {
+			for (var j=0; j<br.length; j++){
+				if (str[i]==br[j][0]){
+					if (br[j][0]!=br[j][1]){
+						arr.push(str[i]);
+}
+					else if (arr[arr.length-1]==br[j][0]) {
+						arr.pop();
+}
+						else {
+							arr.push(str[i]);
+}
+}
+				else if (str[i]==br[j][1]){
+					if (arr[arr.length-1]==br[j][0]){
+						arr.pop();
+}
+					else {
+						arr.push(str[i]);
+						break out;
+}
+}
+
+}
+}
+	if (arr.length>0){
+		return (false);
+}
+	else {
+		return (true);
+}
 }
